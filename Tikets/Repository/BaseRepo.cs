373ce1e16737d1ae42c67115034e627eb441c02c
eslam <eslam.o.osama2003@gmail.com>
2024-11-02 23:26:23 +0200

    improve getAll method

--- Tikets/Repository/BaseRepo.cs
+++ Tikets/Repository/BaseRepo.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 using Tikets.Data;
 using Tikets.Models;
 using Tikets.Repository.IRepository;
@@ -8,26 +9,25 @@ namespace Tikets.Repository
     public class BaseRepo<T> : IBaseRepo<T> where T : class
     {
         ApplicationDbContext _context;
+        DbSet<T> _model;
         public BaseRepo(ApplicationDbContext context)
         {
             _context = context;
+            _model = _context.Set<T>();
         }
-        public List<T> GetAll(string[]? includes = null)
+        public List<T> GetAll(Expression < Func<T, object> >[]? includes)
         {
-            
-
-            var query = _context.Set<T>().AsQueryable();
-
-            if (includes != null)
-            {
-                foreach (var include in includes)
+            var query= _model.AsQueryable();
+            if (includes !=null && includes.Length>0) {
+            foreach(var include in includes)
                 {
                     query = query.Include(include);
                 }
             }
-
-            var items = query.ToList();
-            return items;
+            return query.ToList() ;
+        }
+        public T? GetOne(Expression<Func<T, bool>> expression) {
+            return _model.FirstOrDefault(expression);
         }
     }
 }
--- Tikets/Repository/IRepository/IBaseRepo.cs
+++ Tikets/Repository/IRepository/IBaseRepo.cs
@@ -1,12 +1,13 @@
 ﻿
+using System.Linq.Expressions;
 using Tikets.Models;
 
 namespace Tikets.Repository.IRepository
 {
     public interface IBaseRepo<T> where T : class
     {
-        List<T> GetAll(string[]? include=null);
-        //T? GetOne();
+        List<T> GetAll(Expression<Func<T, bool>> include);
+        T GetOne(Expression<Func<T, bool>> expression);
         //void Update(T entity);
         //void Delete(int id);
     }
