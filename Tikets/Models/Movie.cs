410439f41708ee64ca6a0e3ef2f57ffff4c28f6f
eslam <eslam.o.osama2003@gmail.com>
2024-11-12 01:17:19 +0200

    add movie status in home and details

--- Tikets/Models/Movie.cs
+++ Tikets/Models/Movie.cs
@@ -1,21 +1,21 @@
-﻿namespace Tikets.Models
-{
-    public class Movie
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string Price { get; set; }
-        public string ImgUrl { get; set; }
-        public string TrailerUrl { get; set; }
-        public DateTime startDate { get; set; }
-        public DateTime endDate { get; set; }
-        public MovieStatus MovieStatus { get; set; }
+﻿
+namespace Tikets.Models;
 
-        public int CinemaId { get; set; }
-        public int CategoryId { get; set; }
-        public Cinema Cinema { get; set; }
-        public Category Category { get; set; }
-        public ICollection<ActorMovie> ActorMovies { get; set; }
-    }
+public class Movie
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Price { get; set; }
+    public string ImgUrl { get; set; }
+    public string TrailerUrl { get; set; }
+    public DateTime startDate { get; set; }
+    public DateTime endDate { get; set; }
+    public MovieStatus MovieStatus { get; set; }
+
+    public int CinemaId { get; set; }
+    public int CategoryId { get; set; }
+    public Cinema Cinema { get; set; }
+    public Category Category { get; set; }
+    public ICollection<ActorMovie> ActorMovies { get; set; }
 }
